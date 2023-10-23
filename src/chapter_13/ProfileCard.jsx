92d636d6105982fc5f58028d368ea7ca90cf0ee3
devseunghan <gankyou@naver.com>
2023-10-23 15:48:08 +0900

    20231023 commit

---
+++ src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,10 @@
+import Card from './Card';
+
+export default function ProfileCard(props) {
+  return (
+    <Card title='Seunghah Yoo' backgroundColor="#4ea04e">
+      <p>안녕하세요, 승한입니다.</p>
+      <p>저는 리액트를 사용해서 개발하고 있습니다.</p>
+    </Card>
+  )
+}
